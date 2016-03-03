20de128020b9d7938584503447d9dc6544f57351
Jugg <505490@qq.com>
2016-03-03 14:38:02 +0800

    update flex

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Jugg github demo
+
+### Demo
--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,25 @@
 'use strict';
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var prefix = require('gulp-autoprefixer');
 
 gulp.task('sass',function(){
 return gulp.src('./src/**/*.scss')
 	.pipe(sass())
+	.pipe(prefix())
     .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
     // outputStyle : { expanded , compressed }
 	.pipe(gulp.dest('./dist'))
 })
 
-gulp.task('sass:watch', function() {
+gulp.task('copy',function(){
+	return gulp.src('./src/*.html')
+	  .pipe(gulp.dest('./dist'))
+})
+
+
+gulp.task('default', function() {
     // content
     gulp.watch('src/*.scss', ['sass']);
+    gulp.watch('src/*.html', ['copy']);
 });
